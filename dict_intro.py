d7935fb11d544637bc885541376e372db641f197
Birtalan Etele <etele.birtalan@gmail.com>
2024-12-03 18:44:51 +0200

    changing value in a dictionary

--- dict_intro.py
+++ dict_intro.py
@@ -1,17 +1,20 @@
 vehicles = {
     'dream' : 'Honda 250T',
-    'roadster' : 'BMW R1100',
     'er5' : 'Kawasaki ER5',
     'can-am' : 'Bombardier Van-Am 250',
     'virago' : 'Yamaha XV250',
     'tenere' : 'Yamaha XT650',
     'jimmy' : 'Suzuki Jimmy 1.5',
-    'fiesta' : 'Ford Fiesta Ghia 1.4'
+    'fiesta' : 'Ford Fiesta Ghia 1.4',
+    'roadster' : 'Triumph Street Triple'
 }
 
 vehicles["starfighter"] = "LockheedF-104"
 vehicles["learjet"] = "Bombardier Learjet 75"
 vehicles["toy"] = "glider"
+
+# Update the Virago
+vehicles["virago"] = "Yamaha XV535"
 # for key in vehicles:
 #     print(key, vehicles[key], sep=", ")
 for key, value in vehicles.items():
