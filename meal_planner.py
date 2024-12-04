75191a32c63773fc5bc3912ee2a76ee7a1fc2ce1
Birtalan Etele <etele.birtalan@gmail.com>
2024-12-04 18:56:18 +0200

    "shopping list"

--- meal_planner.py
+++ meal_planner.py
@@ -1,10 +1,35 @@
 from contents import pantry, recipes
 from recipe_options import quantity
 
+def add_shopping_item(data: dict, item: str, amount: int) -> None:
+    """
+    Add an item and its required amount to the shopping list.
+
+    If the item already exists in the shopping list, the function updates the amount
+    by adding the new amount to the existing value. Otherwise, the item is added to the list.
+
+    :param data: The shopping list where items will be added.
+                 This is a list of dictionaries, where each dictionary represents an item
+                 with keys for the item's name and the required amount.
+    :type data: dict
+    :param item: The name of the item to add to the shopping list.
+    :type item: str
+    :param amount: The amount of the item required.
+    :type amount: int
+    :return: None
+    :rtype: None
+    """
+    if item in data:
+        data[item] += amount
+    else:
+        data[item] = amount
+
 # display_dict = {str(index + 1); meal for index, meal in enumerate(recipies)}
 display_dict = {}
 for index, key in enumerate(recipes):
     display_dict[str(index + 1)] = key
+
+shopping_list = {}
 
 while True:
     # Display a menu of the recipes we know how to cook
@@ -30,3 +55,7 @@ while True:
             else:
                 quantity_to_buy = required_quantity - quantity_in_pantry
                 print(f"\tYou need to buy {quantity_to_buy} of {food_item}")
+                add_shopping_item(shopping_list, food_item, quantity_to_buy)
+
+for things in shopping_list.items():
+    print(things)
