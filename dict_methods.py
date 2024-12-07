018bf7d32e3992cf4676144271fe19570b95654e
Birtalan Etele <etele.birtalan@gmail.com>
2024-12-07 14:00:26 +0200

    the dict `update` method

--- dict_methods.py
+++ dict_methods.py
@@ -13,11 +13,26 @@ d = {
 
 pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
 
+d2 = {
+    7: "lucky seven",
+    10: "ten",
+    3: "this is the new three",
+}
+
+d.update(d2)
+for key, value in d.items():
+    print(key, value)
+
+print()
+
+d.update(enumerate(pantry_items))
+for key, value in d.items():
+    print(key, value)
 # new_dict = dict.fromkeys(pantry_items, 0)
 # print(new_dict)
 
-keys = d.keys()
-print(keys)
-
-for item in d.keys():
-    print(item)
+# keys = d.keys()
+# print(keys)
+#
+# for item in d.keys():
+#     print(item)
