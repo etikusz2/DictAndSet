a261507e8725cb4da0faba0ebdb8b91d55e032a3
Birtalan Etele <etele.birtalan@gmail.com>
2024-12-23 18:23:54 +0200

    set update

--- set_union.py
+++ set_union.py
@@ -16,5 +16,7 @@ meds_to_watch = set()
 
 for interaction in adverse_interactions:
     # meds_to_watch = meds_to_watch.union(interaction)
-    meds_to_watch = meds_to_watch | interaction
+    # meds_to_watch = meds_to_watch | interaction
+    # meds_to_watch.update(interaction)
+    meds_to_watch |= interaction
 print(sorted(meds_to_watch))
